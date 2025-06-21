f5cd2f7e6e404ca6bd1e96b0380bdbb9a8af2cd9
Tony2100 <tony1381471821@gmail.com>
2025-06-22 01:51:23 +0800

    feat: update texts and Remove delay when deleting space characters

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const texts = [
+    "[object Object]           哈哈騙你的",
     "每個成功的男人背後，都有一條脊椎",
     "如果你願意多花一點時間了解我，你會發現多花了一點時間",
     "我覺得這件事撇除不好玩的部分，其實都蠻好玩的",
-    "我能預測未來，比如你看完這句話後會看下一句話",
-    "如果你現在在看手機，說明你手機在你手上",
-    "如果覺得冷，就蹲在牆角，因為那裡有90度",
-    "在哪裡跌倒，就在那裡哭",
-    "不努力一下，你怎麼會知道什麼叫做絕望",
+    "注意!!!!!!!!!!!!!     感謝你的注意。",
+    "為什麼警察不直接去監獄裡抓人?",
+    "在成功路上，一定有紅綠燈",
+    "在哪裡跌倒，就在哪裡睡覺",
     "每天喝一罐10%的蘋果汁，10天後你就吃了一顆蘋果",
-    "遇見我是你的榮幸，遇見你是我的不幸",
     "你有什麼不開心的事? 講出來讓大家開心一下嘛",
+    "如果你現在在看手機，代表你手機在你手上",
+    "如果覺得冷，就蹲在牆角，因為那裡有90度",
     "研表究明，手機長期不充電，就會沒電",
     "只要每天省下買一杯奶茶的錢，十天後就能買十杯奶茶",
-    "台灣競爭力低落，在美國就連小學生都會說流利的英語",
-    "經證實，人在清醒的時候通常都沒在睡覺"
+    "經證實，人在清醒的時候通常都沒在睡覺",
+    "什麼是大數據，我只聽過陳樹據",
+    "我能預測未來，比如你看完這句話後會看下一句話"
 ];
 
 const typeSpeed = 80;
@@ -39,7 +41,14 @@ function typeWriter() {
         setTimeout(() => isDeleting = true, pauseTime);
     }
 
-    setTimeout(typeWriter, isDeleting ? backspaceSpeed : typeSpeed);
+    // 如果是 " " (空格) 就不要延遲
+    let delay = typeSpeed;
+    if (isDeleting) {
+        const currentChar = currentText.charAt(charIndex);
+        delay = currentChar === ' ' ? 0 : backspaceSpeed;
+    }
+
+    setTimeout(typeWriter, delay);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
